0822f344628a6b8e78caa30ba4f5043955c638be
anshpanda09 <panda.anshuman002@gmail.com>
2021-07-30 13:09:59 +0530

    initial commit

--- app.js
+++ app.js
@@ -4,8 +4,21 @@ const ejs = require("ejs");
 const _ =require("lodash");
 const mongoose=require("mongoose");
 const https=require("https");
+const bcrypt = require("bcrypt");
+const saltsRound = 10;
 
-mongoose.connect("mongodb+srv://anshpanda09:anshuman123@cluster0-pjrht.mongodb.net/contentDB?retryWrites=true&w=majority",{useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://anshpanda09:anshuman@cluster0.ilijj.mongodb.net/UserDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true  }).then(
+    () => {
+      console.log("Connected");
+    },
+    err => {
+      console.log(err);
+    }
+);
+//var x='';
+
+
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,92 +31,271 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//var posts=[];
-//var post={};
-const postSchema={
-  tittle:String,
-  body:String
-};
-const Post=mongoose.model("Post",postSchema);
-
-
-
-app.get("/",function(req,res)
-{
-  Post.find({},function(err,foundItems){
-    if(!err){
-      res.render("home",{
-        line1:homeStartingContent,
-        posts:foundItems
-
-      });
-    }
-
-  });
-
+app.get("/",(req,res) =>{
+  res.render("wlcome");
+});
+app.get("/sign-up",(req,res) =>{
+  res.render("sign-up",{info:""});
+});
+app.get("/login",(req,res) =>{
+  res.render("login");
 });
 
-app.get('/about',function(req,res){
+
+const userSchema=new mongoose.Schema({
+  username:{
+    type:String,
+    unique:true,
+    required:true
+  },
+  password:{
+    type:String,
+    required:true,
+
+  }
+});
+User=mongoose.model("User",userSchema);
+
+
+
+
+
+app.post("/sign-up",(req,res)=>{
+ let   usNameSign=req.body.usernameSignup;
+  let passWordSign=req.body.passwordSignup;
+  let len = passWordSign.length;
+  if(len <6 && passWordSign===passWordSign.toLowerCase())
+  {
+    res.render("sign-up",{
+      info:"Password length must be greater than 6 and must contains atleast one uppercse letter"
+    });
+  }
+  else
+  {
+    //const query = User.where({username:usNameSign});
+    User.findOne({username:usNameSign},(err,foundUser)=>{
+      if(!err)
+      {
+        if(foundUser)
+        {
+
+          res.render("sign-up",{
+            info:"The user name  already exists.Try another username."
+          });
+
+        }
+        else
+        {
+          bcrypt.hash(passWordSign,saltsRound,function(err,hash){
+            user1=new User({
+              username:usNameSign,
+              password:hash
+            });
+            user1.save(err =>{
+              if(!err)
+              res.redirect("login");
+            });
+          });
+        }
+      }
+    });
+  }
+});
+
+const dataSchema=new mongoose.Schema({
+  name:String,
+  title:{
+    type:String,
+    required:true
+  },
+  body:String
+});
+
+const Data=mongoose.model("Data",dataSchema);
+
+newdata= new Data({
+  name:"anshpanda09",
+  title:"day1",
+  body:"Hello anshpanda09"
+});
+
+//newdata.save();
+var a=[];
+var x='';
+app.post("/login",(req,res) =>{
+  var userNamelog=req.body.usernameLogin;
+
+  var passWordlog=req.body.passwordLogin;
+  //console.log(userNamelog);
+ // console.log(passWordlog);
+  User.findOne({username:userNamelog},(err,foundItem) =>{
+    if(!err)
+    {
+      if(foundItem)
+      {
+        bcrypt.compare(passWordlog, foundItem.password ,function(err,result){
+          if(result)
+          {
+            x=userNamelog;
+            Data.find({name:userNamelog},(err,foundUsers) =>{
+               if(!err)
+               {
+                 if(foundUsers)
+                 {
+                   a=foundUsers;
+                   res.render("home",{
+                     line1:homeStartingContent,
+                     posts:foundUsers});
+                 }
+                 else{
+                   var arr=[];
+                   res.render("home",{posts:arr});
+                 }
+               }
+            }) ;
+          }
+          else{
+            console.log("Incorrect Password");
+            res.redirect("login");
+          }
+        });
+
+      }
+      else{
+        console.log("Invalid Username");
+        res.redirect("login");
+      }
+    }
+  });
+});
+
+
+app.get("/about",(req,res)=>{
   res.render("about",{
     content:aboutContent
   });
 });
 
-app.get("/contact",function(req,res){
+
+app.get("/contact",(req,res)=>{
   res.render("contact",{
     contact:contactContent
   });
 });
 
-app.get('/compose',function(req,res){
+
+app.get("/home",(req,res)=>{
+  Data.find({name:x},(err,foundItems)=>{
+    if(!err)
+    {
+      res.render("home",{
+        line1:homeStartingContent,
+        posts:foundItems
+      });
+
+    }
+  })
+
+});
+
+
+
+app.get("/compose",(req,res) =>{
   res.render("compose");
 });
 
+app.post("/compose",(req,res)=>{
+  var title=req.body.title;
+  var body=req.body.body;
+  post=new Data({
+    name:x,
+    title:title,
+    body:body
+  });
+  post.save(err=>{
+    if(!err)
+    Data.find({name:x},(err,Items)=>{
+      if(!err)
+      {
+        res.render("home",{
+          line1:homeStartingContent,
+          posts:Items
+        });
+      }
+    });
+  });
 
-app.post("/compose",function(req,res){
-
-
-   var post= new Post({
-  tittle:req.body.tittle,
-  body:req.body.body
 });
 
- var requestedItems=_.lowerCase(req.body.tittle);
-Post.findOne({tittle:req.body.tittle},function(err,foundItems){
-  if(!err)
-  {
-    if(!foundItems){
-      post.save();
-    }
-  }
-});
 
-res.redirect("/");
+app.post("/delete",(req,res) =>{
 
+  let delTitle=req.body.button;
+  let delUser=x;
+  Data.deleteOne({name:delUser,title:delTitle},(err)=>{
+    if(!err)
+    {
+     // console.log(delTitle);
+     // console.log(delUser);
+      console.log("successfully deleted");
+      Data.find({name:x},(err,Items) =>{
+        if(!err)
+        {
+          res.render("home",{
+            line1:homeStartingContent,
+            posts:Items
+          });
+        }
+      });
 
-});
-
-app.get("/posts/:postName",function(req,res){
-  var reqTittle=_.lowerCase(req.params.postName);
-  posts.forEach(function(post){
-    var storedTittle=_.lowerCase(post.tittle);
-    if(storedTittle=== reqTittle){
-    res.render("post",{
-      tittle:post.tittle,
-      content:post.body
-    })  ;
     }
   });
 });
 
-app.post("/delete",function(req,res){
-  topic=req.body.button;
-  Post.deleteOne({tittle:topic},function(err){
-    if(err){
-      console.log(err);
+var  editUser='';
+var editTitle='';
+
+app.get("/edit",(req,res)=>{
+  res.render("edit",{
+    title:"",
+    bodyy:""
+  });
+});
+
+app.post("/edit",(req,res) =>{
+     editUser=x;
+     editTitle=req.body.button1;
+
+  Data.findOne({name:editUser,title:editTitle},(err,Item)=>{
+    if(!err)
+    {
+      res.render("edit",{
+        title:Item.title,
+        bodyy:Item.body
+      });
     }
   });
-  res.redirect("/");
+
+});
+
+app.post("/change",(req,res)=>{
+  let editedTitle=req.body.title;
+  let editedBody=req.body.body;
+
+  Data.findOneAndUpdate({name:x,title:editTitle},{title:editedTitle,body:editedBody},(err)=>{
+    if(!err)
+    {
+      Data.find({name:x},(err,Items)=>{
+        if(!err)
+        {
+          res.render("home",{
+            line1:homeStartingContent,
+            posts:Items
+          });
+        }
+      });
+    }
+  });
 
 });
 
@@ -119,6 +311,13 @@ app.post("/delete",function(req,res){
 
 
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+
+
+
+
+
+
+//creating server
+app.listen(process.env.PORT || 3000,() =>{
+  console.log("Server stated on port 3000");
 });
